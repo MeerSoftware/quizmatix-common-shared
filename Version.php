c3493192b4fad7c80552d3e492166a860679e438
bariscodefx <bariscodefx@protonmail.com>
2023-12-18 21:02:01 +0300

    Create Version.php

---
+++ Version.php
@@ -0,0 +1,18 @@
+<?php
+
+// Copyright (C) 2023 meerSoftware Version.php by bariscodefx
+// version info file
+
+// Version name
+$_VERSION = "v1.035";
+
+// keep the real version data (for debug)
+$_REAL_VERSION = $_VERSION;
+
+// release type
+$_RELEASE_TYPE = "debug";
+
+// if release type is debug set version to dynamic style for load everytime assets files
+if($_RELEASE_TYPE === "debug") {
+    $_VERSION = "dev-" . time();
+} // made for realtime cookies
